4985be331031c066018b22db201b5b9797fb0b51
lengyijun <sjtu5140809011@gmail.com>
2024-12-06 17:16:29 +0800

    theme

--- src/bin/review.rs
+++ src/bin/review.rs
@@ -1,3 +1,4 @@
+use cursive::style::{BorderStyle, Palette};
 use cursive::traits::*;
 use cursive::views::Dialog;
 use cursive::views::TextView;
@@ -19,6 +20,37 @@ async fn main() {
         return;
     };
     siv.set_user_data(history);
+
+    // Start with a nicer theme than default
+    siv.set_theme(cursive::theme::Theme {
+        shadow: true,
+        borders: BorderStyle::Simple,
+        palette: Palette::retro().with(|palette| {
+            use cursive::style::BaseColor::*;
+
+            {
+                // First, override some colors from the base palette.
+                use cursive::style::Color::TerminalDefault;
+                use cursive::style::PaletteColor::*;
+
+                palette[Background] = TerminalDefault;
+                palette[View] = TerminalDefault;
+                palette[Primary] = White.dark();
+                palette[TitlePrimary] = Blue.light();
+                palette[Secondary] = Blue.light();
+                palette[Highlight] = Blue.dark();
+            }
+
+            {
+                // Then override some styles.
+                use cursive::style::Effect::*;
+                use cursive::style::PaletteStyle::*;
+                use cursive::style::Style;
+                palette[Highlight] = Style::from(Blue.light()).combine(Bold);
+                palette[EditableTextCursor] = Style::secondary().combine(Reverse).combine(Underline)
+            }
+        }),
+    });
 
     // Create a linear layout to arrange buttons horizontally at the bottom
     // let button_layout = LinearLayout::horizontal()

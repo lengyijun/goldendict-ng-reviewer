45ac0a143dfb27858fc2e43adc692de4044f8b27
lengyijun <sjtu5140809011@gmail.com>
2024-12-06 13:42:38 +0800

    wip

--- src/bin/review.rs
+++ src/bin/review.rs
@@ -28,38 +28,48 @@ fn main() {
                 s.add_layer(Dialog::info("Good selected"));
                 s.call_on_name("ocean", |view: &mut Dialog| {
                     view.clear_buttons();
+
                     let word = view.get_title().to_owned();
                     view.add_button("Easy", move |s: &mut Cursive| {
-                        let next_word = s.with_user_data(|history: &mut SQLiteHistory| {
-                            let _ = block_on(history.update(&word, Rating::Easy));
-                            block_on(history.next_to_review())
-                        });
-                        match next_word {
-                            Some(Ok(next_word)) => {
-                                s.call_on_name("ocean", |view: &mut Dialog| {
-                                    view.set_title(next_word);
-                                });
-                            }
-                            _ => {
-                                s.quit();
-                            }
-                        }
+                        update_and_review_next(s, word.clone(), Rating::Easy);
                     });
-                    view.add_button("Good", |s| {
-                        s.add_layer(Dialog::info("Good selected"));
+
+                    let word = view.get_title().to_owned();
+                    view.add_button("Good", move |s| {
+                        update_and_review_next(s, word.clone(), Rating::Good);
                     });
-                    view.add_button("Hard", |s| {
-                        s.add_layer(Dialog::info("Hard selected"));
+
+                    let word = view.get_title().to_owned();
+                    view.add_button("Hard", move |s| {
+                        update_and_review_next(s, word.clone(), Rating::Hard);
                     });
-                    view.add_button("Again", |s| {
-                        s.add_layer(Dialog::info("Again selected"));
+
+                    let word = view.get_title().to_owned();
+                    view.add_button("Again", move |s| {
+                        update_and_review_next(s, word.clone(), Rating::Again);
                     });
                 });
             })
             .with_name("ocean"), // .content(button_layout)
-                                 // .button("Quit", |s| s.quit()),
                                  // .padding(Margins::lrtb(10, 10, 0, 35))
     );
 
     siv.run();
+}
+
+fn update_and_review_next(s: &mut Cursive, word: String, rating: Rating) {
+    let next_word = s.with_user_data(|history: &mut SQLiteHistory| {
+        let _ = block_on(history.update(&word, rating));
+        block_on(history.next_to_review())
+    });
+    match next_word {
+        Some(Ok(next_word)) => {
+            s.call_on_name("ocean", |view: &mut Dialog| {
+                view.set_title(next_word);
+            });
+        }
+        _ => {
+            s.quit();
+        }
+    }
 }

c48838a33bfe42f9e317e1187047084ebb2cf34b
lengyijun <sjtu5140809011@gmail.com>
2024-12-06 13:22:49 +0800

    not sure

--- src/bin/review.rs
+++ src/bin/review.rs
@@ -1,10 +1,17 @@
+#![feature(async_closure)]
+
 use cursive::traits::*;
 use cursive::views::Dialog;
 use cursive::Cursive;
 use cursive::CursiveExt;
+use futures::executor::block_on;
+use mdict_cli_rs::fsrs::sqlite_history::SQLiteHistory;
+use mdict_cli_rs::spaced_repetition::SpacedRepetition;
+use rs_fsrs::Rating;
 
 fn main() {
     let mut siv = Cursive::default();
+    siv.set_user_data(SQLiteHistory::default());
 
     // Create a linear layout to arrange buttons horizontally at the bottom
     // let button_layout = LinearLayout::horizontal()
@@ -20,10 +27,23 @@ fn main() {
             .button("Show answer", |s| {
                 s.add_layer(Dialog::info("Good selected"));
                 s.call_on_name("ocean", |view: &mut Dialog| {
-                    view.set_title("132");
                     view.clear_buttons();
-                    view.add_button("Easy", |s| {
-                        s.add_layer(Dialog::info("Easy selected"));
+                    let word = view.get_title().to_owned();
+                    view.add_button("Easy", move |s: &mut Cursive| {
+                        let next_word = s.with_user_data(|history: &mut SQLiteHistory| {
+                            let _ = block_on(history.update(&word, Rating::Easy));
+                            block_on(history.next_to_review())
+                        });
+                        match next_word {
+                            Some(Ok(next_word)) => {
+                                s.call_on_name("ocean", |view: &mut Dialog| {
+                                    view.set_title(next_word);
+                                });
+                            }
+                            _ => {
+                                s.quit();
+                            }
+                        }
                     });
                     view.add_button("Good", |s| {
                         s.add_layer(Dialog::info("Good selected"));

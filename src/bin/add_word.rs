73a09693b45e8871c4bb8fda8c2bdd9f93ac8704
lengyijun <sjtu5140809011@gmail.com>
2025-03-19 07:41:54 +0800

    add_word: insert every arg
    rustc 1.87.0-nightly (f9e0239a7 2025-03-04)

--- src/bin/add_word.rs
+++ src/bin/add_word.rs
@@ -8,14 +8,16 @@ async fn main() -> Result<()> {
     let w = args().nth(1).unwrap_or("--help".to_owned());
     if w == "--help" {
         println!("used in goldendict-ng, program dic");
-        println!("add first argument to sqlite");
+        println!("add all arguments to sqlite");
         println!("https://github.com/lengyijun/goldendict-ng-helper");
         return Ok(());
     }
     let mut sqlite_history = SQLiteHistory::default().await;
-    let card = get_word(&sqlite_history.conn, &w)
-        .await
-        .unwrap_or(Card::new());
-    sqlite_history.insert_or_replace(&w, card).await?;
+    for w in args().skip(1) {
+        let card = get_word(&sqlite_history.conn, &w)
+            .await
+            .unwrap_or(Card::new());
+        sqlite_history.insert_or_replace(&w, card).await?;
+    }
     Ok(())
 }

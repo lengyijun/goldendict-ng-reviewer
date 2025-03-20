d2a0e60b59b31be2ac9a6039d182599fa06cc9a9
lengyijun <sjtu5140809011@gmail.com>
2025-03-20 15:49:30 +0800

    review favourites
    rustc 1.87.0-nightly (f9e0239a7 2025-03-04)

---
+++ Makefile
@@ -0,0 +1,3 @@
+install:
+	cargo install --path .
+	cp review.fish ~/.config/fish/completions/
---
+++ review.fish
@@ -0,0 +1,6 @@
+complete -c review -l help           -d 'Print the help message.' -f
+
+set -l subcommands favourite (cat ~/.config/goldendict/favorites | grep folder | grep expanded | grep -oP '(?<=name=").+?(?=")')
+
+complete -f -c review -a (string join " " $subcommands)
+
